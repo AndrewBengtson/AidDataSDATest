8bf82324135ae76fe0086b7f613376d588a09b0e
agben <Ya13457889>
2021-04-24 16:31:56 -0400

    Comparator methods implemented

--- src/entities/XComparator.cpp
+++ src/entities/XComparator.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "entities/XComparator.h"
+
+bool XComparator::lessThan(Coordinate &c1, Coordinate &c2) {
+    return c1.getX()<c2.getX();
+}
+
+bool XComparator::equals(Coordinate &c1, Coordinate &c2) {
+    return c1.getY()==c1.getX() && c1.getY()==c2.getY() && c1.isInA() == c2.isInA();
+}
--- src/entities/YComparator.cpp
+++ src/entities/YComparator.cpp
@@ -3,3 +3,11 @@
 //
 
 #include "entities/YComparator.h"
+
+bool YComparator::lessThan(Coordinate &c1, Coordinate &c2) {
+    return c1.getY()<c2.getY();
+}
+
+bool YComparator::equals(Coordinate &c1, Coordinate &c2) {
+    return c1.getY()==c1.getX() && c1.getY()==c2.getY() && c1.isInA() == c2.isInA();
+}
