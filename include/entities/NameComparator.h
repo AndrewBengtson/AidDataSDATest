e1b24a7d99f4129fba468b9ac91463e6b998498d
agben <Ya13457889>
2021-04-25 09:28:24 -0400

    Helper class to sort golfers by name

---
+++ include/entities/NameComparator.h
@@ -0,0 +1,16 @@
+//
+// Created by agben on 4/25/2021.
+//
+
+#ifndef AIDDATASDATEST_NAMECOMPARATOR_H
+#define AIDDATASDATEST_NAMECOMPARATOR_H
+
+//this class is used to sort golfers by their names
+class NameComparator : public Comparator<Golfer>{
+public:
+    //this compares golfers only by their names, which will sort golfers with the same name together
+    bool lessThan(Golfer &g1, Golfer &g2) override ;
+};
+
+
+#endif //AIDDATASDATEST_NAMECOMPARATOR_H
---
+++ src/entities/NameComparator.cpp
@@ -0,0 +1,10 @@
+//
+// Created by agben on 4/25/2021.
+//
+
+#include "entities/NameComparator.h"
+
+bool NameComparator::lessThan(Golfer &g1, Golfer &g2) {
+    //simply calls string.lessThan
+    return g1.getName()<g2.getName();
+}
